5a3d52df6daeddf17795dbe3a6d2f4d8365c28f0
iskay <ikay@lakeheadu.ca>
2023-06-13 20:36:00 +0000

    add 5050/udp to docker compose

--- app/data/config/mainnet-go-opera/start-node.sh
+++ app/data/config/mainnet-go-opera/start-node.sh
@@ -3,5 +3,6 @@
 # download genesis file
 wget https://download.fantom.network/mainnet-109331-no-history.g
 
-./opera --genesis=mainnet-109331-no-history.g --db.preset ldb-1 --syncmode snap --http --http.addr="0.0.0.0" --http.corsdomain="*" --http.api=eth,web3,net,txpool,ftm
+#./opera --genesis=mainnet-109331-no-history.g --db.preset ldb-1 --syncmode snap --http --http.addr="0.0.0.0" --http.corsdomain="*" --http.api=eth,web3,net,txpool,ftm
+./opera --genesis=mainnet-109331-no-history.g --db.preset ldb-1 --syncmode snap --http --http.addr="0.0.0.0" --http.corsdomain="*" --http.api=eth,web3,net,txpool,ftm --ws --ws.origins="*" --ws.addr="0.0.0.0" --ws.api=eth,web3,net,ftm --cache 8192
 #tail -f /dev/null
