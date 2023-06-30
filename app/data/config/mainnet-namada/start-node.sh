67e2d504be058facab7709c0daa3b1629f279389
iskay <ikay@lakeheadu.ca>
2023-06-30 18:35:26 +0000

    add internal host to cors

--- app/data/config/mainnet-namada/start-node.sh
+++ app/data/config/mainnet-namada/start-node.sh
@@ -10,4 +10,4 @@ fi
 EXTIP=${EXTIP:-""}
 namada client utils join-network --chain-id $CHAIN_ID
 #CMT_LOG_LEVEL=p2p:none,pex:error namada node ledger run
-NAMADA_LOG=info CMT_LOG_LEVEL=p2p:none,pex:error NAMADA_CMT_STDOUT=true NAMADA_LEDGER__COMETBFT__P2P_EXTERNAL_ADDRESS=true namada node ledger run
+NAMADA_LOG=info CMT_LOG_LEVEL=p2p:none,pex:error NAMADA_CMT_STDOUT=true NAMADA_LEDGER__COMETBFT__P2P_EXTERNAL_ADDRESS=$EXTIP NAMADA_LEDGER__COMETBFT__RPC_LADDR=tcp://0.0.0.0:26657 NAMADA_LEDGER__COMETBFT__RPC_CORS_ALLOWED_ORIGINS=host.docker.internal node ledger run
