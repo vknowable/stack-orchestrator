d3aab142d5ca6cbb0b4f00a9d3fbbcaa3863a6fb
iskay <ikay@lakeheadu.ca>
2023-08-06 01:22:41 +0000

    set config server and build wasm locally

--- app/data/config/public-namada/start-node.sh
+++ app/data/config/public-namada/start-node.sh
@@ -20,7 +20,17 @@ echo "Using Chain Id: $CHAIN_ID"
 EXTIP=${EXTIP:-""}
 
 # Download chain configs and init directories
-namada client utils join-network --chain-id $CHAIN_ID
+if [ -z "$CONFIGS_SERVER" ]; then
+  # Attempt to download configs and wasm from Heliax repo (default behaviour)
+  namada client utils join-network --chain-id $CHAIN_ID
+else
+  # Use alternate configs server if provided (eg: internal testnet)
+  export NAMADA_NETWORK_CONFIGS_SERVER="${CONFIGS_SERVER}"
+  namada client utils join-network --chain-id $CHAIN_ID --dont-prefetch-wasm
+  # copy wasm to namada dir
+  cp -a /app/namada/wasm/*.wasm /root/.local/share/namada/$CHAIN_ID/wasm
+  cp -a /app/namada/wasm/checksums.json /root/.local/share/namada/$CHAIN_ID/wasm
+fi
 
 # start the node
 NAMADA_LOG=info \
@@ -28,7 +38,8 @@ CMT_LOG_LEVEL=p2p:none,pex:error \
 NAMADA_CMT_STDOUT=true \
 NAMADA_LEDGER__COMETBFT__P2P_EXTERNAL_ADDRESS=$EXTIP \
 NAMADA_LEDGER__COMETBFT__RPC__LADDR="${RPC_LISTEN_ADDR:-tcp://127.0.0.1:26657}" \
-NAMADA_LEDGER__COMETBFT__RPC__CORS_ALLOWED_ORIGINS="{$RPC_CORS_ALLOWED:-[]}" \
+NAMADA_LEDGER__COMETBFT__RPC__CORS_ALLOWED_ORIGINS="${RPC_CORS_ALLOWED:-[]}" \
 NAMADA_LEDGER__COMETBFT__TX_INDEX="${INDEXER:-null}" \
-NAMADA_LEDGER__COMETBFT__INSTRUMENTATION__PROMETHEUS="${PROMETHEUS_ENABLE:-true}" \
+NAMADA_LEDGER__COMETBFT__INSTRUMENTATION__PROMETHEUS="${PROM_ENABLE:-true}" \
+NAMADA_LEDGER__COMETBFT__INSTRUMENTATION__PROMETHEUS_LISTEN_ADDR=":${PROM_PORT:-26660}" \
 namada node ledger run
