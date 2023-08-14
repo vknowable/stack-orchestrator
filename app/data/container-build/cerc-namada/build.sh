2d1a46fdab9a842e461cd992c5367097d968bedc
Ian <ikay@lakeheadu.ca>
2023-08-14 14:19:32 -0400

    Namada (#4)
    * first commit
    
    * full node stack
    
    * uncomment SCRIPT_DIR env variable
    
    * add internal host to cors
    
    * add internal host to compose
    
    * revert internal host cors
    
    * first commit
    
    * full node stack
    
    * uncomment SCRIPT_DIR env variable
    
    * add internal host to cors
    
    * add internal host to compose
    
    * revert internal host cors
    
    * runtime options env vars
    
    * remove duplicate files
    
    * set config server and build wasm locally
    
    * sed configs
    
    * add readme
    
    * merge with main
    
    * cleanup duplicates
    
    * config persistent_peers
    
    * fix RPC_LISTEN script condition
    
    * update readme
    
    * remove setup-repositories step
    
    * add quickstart
    
    * update readmes
    
    * update readme
    
    * Update and rename quickstart.md to digitalocean_quickstart.md
    
    * typo
    
    * Update README.md
    
    * Update digitalocean_quickstart.md
    
    * Update start-node.sh
    
    * remove stray env file
    
    ---------
    
    Co-authored-by: Gavin Birch <13985253+gavinly@users.noreply.github.com>

---
+++ app/data/container-build/cerc-namada/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Build cerc/go-opera
+source ${CERC_CONTAINER_BASE_DIR}/build-base.sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "build_command_args: $build_command_args"
+
+docker build -t cerc/namada:local -f ${SCRIPT_DIR}/Dockerfile ${build_command_args} ${SCRIPT_DIR}
