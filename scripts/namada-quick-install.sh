bd5fd47231c574b8db130b586b92ea768a7bc350
Ian <ikay@lakeheadu.ca>
2023-09-26 10:25:20 -0400

    update install script to find correct python3-venv version for system.sh

--- scripts/namada-quick-install.sh
+++ scripts/namada-quick-install.sh
@@ -3,8 +3,13 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_DIR=${SCRIPT_DIR}/..
 
+# get installed python version
+PYTHON3_VERSION=$(python3 --version 2>&1 | awk '{split($2, a, "."); print a[1]"."a[2]}')
+
 sudo apt update
-sudo apt install python3-pip python3.10-venv -y
+sudo apt upgrade -y
+#sudo apt install python3-pip python3.10-venv -y
+sudo apt install python3-pip python${PYTHON3_VERSION}-venv -y
 
 # install stack-orchestrator dependencies
 sudo ${REPO_DIR}/scripts/quick-install-ubuntu.sh -y
